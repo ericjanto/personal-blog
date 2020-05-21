ad5cc9a89d163bf7a0b592c64069eec5045b1789
55640281 <55640281+ericjanto@users.noreply.github.com>
2020-05-21 15:29:04 +0200

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import Helmet from 'react-helmet'
 import ThemeContext from '../context/ThemeContext'
 import Layout from '../layout'
 import SEO from '../components/SEO'
-import config from '../../data/SiteConfig'
+import {NotFoundBox} from '../components/NotFoundBox';
+
 
 export default class NotFoundPage extends Component {
   static contextType = ThemeContext
@@ -22,31 +22,10 @@ export default class NotFoundPage extends Component {
 
   render() {
     return (
-      <Layout>
-        <Helmet title={`Page not found – ${config.siteTitle}`} />
-        <SEO />
-        <div className="container">
-          <div className="text-center">
-            <h1>404</h1>
-          </div>
-          <p>
-            A fatal exception 0E has occurred at <span className="tania">0x74616e6961</span> in 404:
-            page not found.
-          </p>
-          <div className="list">
-            <p>
-              <span className="bullet">*</span> Click any link to terminate the current application.
-            </p>
-            <p>
-              <span className="bullet">*</span> Press ALT + F4 again to restart your browser. You
-              will lose any unsaved information in all tabs.
-            </p>
-          </div>
-          <p className="text-right">
-            Click any link to continue<span className="blink">&#9608;</span>
-          </p>
-        </div>
+      <Layout simple>
+        <SEO title="404: Not found" />
+        <NotFoundBox />
       </Layout>
-    )
+  )
   }
 }
