02353c5fb6b2e9a42976664157fd626413f85ed2
55640281 <55640281+ericjanto@users.noreply.github.com>
2020-05-24 09:34:15 +0200

    update time range of 'new'

--- src/components/PostListing.js
+++ src/components/PostListing.js
@@ -37,7 +37,7 @@ export default class PostListing extends Component {
 
           const popular = post.categories.includes('Popular')
           const date = formatDate(post.date)
-          const newest = moment(post.date) > moment().subtract(1, 'months')
+          const newest = moment(post.date) > moment().subtract(14, 'days')
 
           return (
             <Link to={post.path} key={post.title}>
