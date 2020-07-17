674cf4b9c694e84a56c7f19ef898c42cff3c16a5
55640281 <55640281+ericjanto@users.noreply.github.com>
2020-07-17 13:47:57 +0200

    add SAP project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,12 @@
 export default [
   {
+    icon: '😷',
+    title: 'Corona-Warn-App',
+    path: 'https://github.com/corona-warn-app',
+    description:
+      'The official COVID-19 exposure notification app for Germany. Systems & UI testing team.',
+  },
+  {
     icon: '🎶',
     title: 'EUSO Website',
     path: '/euso-project',
