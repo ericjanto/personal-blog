1712437d919926f8f87b642dbe68ca7af099133e
55640281 <55640281+ericjanto@users.noreply.github.com>
2020-12-31 19:42:39 +0100

    lazy loading

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Gallery from 'react-photo-gallery'
-import useDimensions from 'react-cool-dimensions'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
@@ -11,22 +9,11 @@ import DynamicLoadingGallery from '../components/DynamicLoadingGallery'
 
 
 const GalleryPage = () => {
-  const { ref, width, height } = useDimensions({
-    useBorderBoxSize: true,
-  });
-
   return (
     <Layout>
       <Helmet title={`Photography | ${config.siteTitle}`} />
       <SEO />
-      {/* <div ref={ref}>
-        {width >= 675 && (
-          <Gallery photos={photos} direction={"column"} columns={2} margin={8} />
-          )}
-        {width < 675 && (
-          <Gallery photos={photos} direction={"column"} columns={1} margin={8} />
-        )}
-      </div> */}
+      {/* TODO: Add progressive image loading */}
       <DynamicLoadingGallery photos={photos} />
     </Layout>
   );
