4d054c5322332ed5167f67d22e255e8b9ce2311b
Eric Janto <e.janto@gmx.de>
2021-09-04 01:26:25 +0100

    get rid of replaceAll

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -70,7 +70,7 @@ export function catPrettyPrint(tag) {
   console.log(words)
 
   for (let i = 0; i < words.length; i++) {
-    words[i] = words[i].replaceAll(/"/g,"")
+    words[i] = words[i].replace(/"/g,'')
     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
   }
 
