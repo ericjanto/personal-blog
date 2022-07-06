50b99947350ae96a6201eadc001cd9b0419c5b57
Eric Janto <e.janto@gmx.de>
2022-07-06 18:16:16 +0200

    Update config content

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 export default {
   siteTitle: 'Eric Janto',
   siteUrl: 'https://www.ericjanto.com',
-  description: 'Welcome to my personal website. This website contains a project showcase, various writings about computer science, and more.',
+  description:
+    'Welcome to my personal website. This website contains a project showcase, various writings about computer science, and more',
   userTwitter: 'ericjnto',
 }
